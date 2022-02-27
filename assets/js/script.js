972d72f1600cb1a0616a3ba5dd63b62295c5e0d7
Okens <Ng.Eric314@gmail.com>
2022-02-26 22:35:03 -0800

    Adding saveStorage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
 console.log("test")
+
+$(document).ready(function(){
+ console.log("test2");
+})
+
+function clickSaveScore() {
+  saveStorage();
+}
+
+function updateTaskList(){
+
+
+}
+
+function getStorage(){
+
+
+}
+
+function saveStorage(){
+ var currentStorage = JSON.parse(localStorage.getItem('taskList'));
+ if(currentStorage === null){
+   currentStorage = []
+ }
+
+ currentStorage.push({'time': time, 'task': task})
+ localStorage.setItem('taskList', JSON.stringify(currentStorage));
+ updateTaskList();
+}
