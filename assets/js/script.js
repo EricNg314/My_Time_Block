68ca22959856648b35d1d5e526ed1a2fb6ea0d2b
Okens <Ng.Eric314@gmail.com>
2022-02-27 02:46:11 -0800

    Adding renderRow function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 var taskContainer = $("#timeBlockContainer");
 
-var timeSlots = getStorage();
-console.log(timeSlots);
-for (var i = 0; i < timeSlots.length; i++) {
-  console.log("current time slots");
+var defaultTimeSlots = [
+  { time: "9:00 AM", task: "abd" },
+  { time: "10:00 AM", task: "qwe" },
+  { time: "11:00 AM", task: "" },
+  { time: "12:00 PM", task: "oijafe" },
+  { time: "1:00 PM", task: "" },
+  { time: "2:00 PM", task: "" },
+  { time: "3:00 PM", task: "" },
+  { time: "4:00 PM", task: "" },
+  { time: "5:00 PM", task: "" },
+];
+var timeTaskStorage = getStorage();
 
-}
+displayTaskList();
 
 $(document).ready(function () {
-  console.log("test2");
-  taskContainer.on('click', 'button.saveBtn', function(){
+  taskContainer.on("click", "button.saveBtn", function () {
     clickSaveTask();
-  })
+  });
 });
 
 function clickSaveTask() {
@@ -19,39 +26,62 @@ function clickSaveTask() {
   var task = "";
   saveTask();
   saveStorage();
-  updateTaskList();
+  // displayTaskList();
+
+
+  // renderRow(time, task)
 }
 
-function saveTask(){
-  // TODO: Update the timeSlots object.  
+function saveTask() {
+  // TODO: Update the timeSlots object.
   // var d = new Date();
   // d.getHours();
+  // renderRow()
 }
 
-function updateTaskList() {
+function displayTaskList() {
+  // var currentStorage = getStorage();
 
-  var currentStorage = getStorage();
-  taskContainer.innerHTML = "";
-
-  if (currentStorage !== null) {
     // Add some date.
-
-    // Append each row of saved storage
-    for (var i = 0; i < currentStorage.length; i++) {
-      var timeRowEle = $("<div>");
-      var labelEle = $("<label>");
-      var textareaEle = $("<textarea>");
-      var buttonEle = $("<button>");
-
-      labelEle.textContent = currentStorage[i].time;
-      textareaEle.textContent = currentStorage[i].task;
-      buttonEle.textContent = "";
-
-      labelEle.appendTo(timeRowEle);
-      textareaEle.appendTo(timeRowEle);
-      buttonEle.appendTo(timeRowEle);
-      timeRowEle.appendTo(taskContainer);
+    for(var i = 0; i < defaultTimeSlots.length; i++){
+      renderRow(defaultTimeSlots[i].time,defaultTimeSlots[i].task)
     }
+ 
+}
+
+function renderRow(time, task){
+  
+  console.log("renderRow")
+  var timeRowSearch = $(`li[data-time='${time}']`)
+  var timeRowEle = ""
+  var labelEle = ""
+  var textareaEle = ""
+  var buttonEle = ""
+  var rowExists = false;
+
+  console.log("timeRowSearch", timeRowSearch)
+  
+    // if the row element does not exist, then create new one, else assign
+    // Helps render content once.
+  if(timeRowSearch.length === 0){
+    rowExists = false;
+    timeRowEle = $(`<li class="row color-bg-secondary form-floating time-block " data-time="${time}">`);
+    labelEle = $(`<label class="col-2 col-lg-1 p-3 hour " >${time}</label>`);
+    textareaEle = $(`<textarea class="col-8 col-lg-10 form-control" placeholder="" >${task}</textarea>`);
+    buttonEle = $('<button class="col-2 col-lg-1 saveBtn"><i class="bi-calendar-plus fa-lg"></i></button>');
+  } else {
+    rowExists = true;
+    textareaEle = timeRowSearch.find('textarea')
+    textareaEle.val(task)
+  }
+
+  console.log("timeRowEle", timeRowEle)
+
+  if(rowExists === false){
+    labelEle.appendTo(timeRowEle);
+    textareaEle.appendTo(timeRowEle);
+    buttonEle.appendTo(timeRowEle);
+    timeRowEle.appendTo(taskContainer);
   }
 }
 
@@ -71,7 +101,7 @@ function saveStorage() {
     currentStorage.sort(compareDescending);
   }
   localStorage.setItem("taskList", JSON.stringify(currentStorage));
-  updateTaskList();
+  displayTaskList();
 }
 
 function compareDescending(a, b) {
