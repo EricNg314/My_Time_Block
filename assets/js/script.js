32b7e6a73381fe8fbe0f30542fe5bc9b53a86a7b
Okens <Ng.Eric314@gmail.com>
2022-02-26 23:43:44 -0800

    Adding save storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,86 @@
-console.log("test")
+var taskContainer = $("#timeBlockContainer");
 
-$(document).ready(function(){
- console.log("test2");
-})
+var timeSlots = getStorage();
+console.log(timeSlots);
+for (var i = 0; i < timeSlots.length; i++) {
+  console.log("current time slots");
 
-function clickSaveScore() {
+}
+
+$(document).ready(function () {
+  console.log("test2");
+  taskContainer.on('click', 'button.saveBtn', function(){
+    clickSaveTask();
+  })
+});
+
+function clickSaveTask() {
+  var time = "";
+  var task = "";
+  saveTask();
   saveStorage();
+  updateTaskList();
 }
 
-function updateTaskList(){
-
-
+function saveTask(){
+  // TODO: Update the timeSlots object.  
+  // var d = new Date();
+  // d.getHours();
 }
 
-function getStorage(){
+function updateTaskList() {
 
+  var currentStorage = getStorage();
+  taskContainer.innerHTML = "";
 
+  if (currentStorage !== null) {
+    // Add some date.
+
+    // Append each row of saved storage
+    for (var i = 0; i < currentStorage.length; i++) {
+      var timeRowEle = $("<div>");
+      var labelEle = $("<label>");
+      var textareaEle = $("<textarea>");
+      var buttonEle = $("<button>");
+
+      labelEle.textContent = currentStorage[i].time;
+      textareaEle.textContent = currentStorage[i].task;
+      buttonEle.textContent = "";
+
+      labelEle.appendTo(timeRowEle);
+      textareaEle.appendTo(timeRowEle);
+      buttonEle.appendTo(timeRowEle);
+      timeRowEle.appendTo(taskContainer);
+    }
+  }
 }
 
-function saveStorage(){
- var currentStorage = JSON.parse(localStorage.getItem('taskList'));
- if(currentStorage === null){
-   currentStorage = []
- }
+function getStorage() {
+  var storageData = JSON.parse(localStorage.getItem("taskList"));
+  if (storageData === null) {
+    storageData = [];
+  }
 
- currentStorage.push({'time': time, 'task': task})
- localStorage.setItem('taskList', JSON.stringify(currentStorage));
- updateTaskList();
+  return storageData;
+}
+
+function saveStorage() {
+  var currentStorage = timeSlots;
+
+  if (currentStorage.length > 1) {
+    currentStorage.sort(compareDescending);
+  }
+  localStorage.setItem("taskList", JSON.stringify(currentStorage));
+  updateTaskList();
+}
+
+function compareDescending(a, b) {
+  // sort current object and next object by time key
+  if (a.time > b.time) {
+    return -1;
+  }
+  if (a.time < b.time) {
+    return 1;
+  }
+  return 0;
 }
