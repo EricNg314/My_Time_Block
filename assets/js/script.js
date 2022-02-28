ac05b4af327ab6215805577bb9dcc1bbc33f2b20
Okens <Ng.Eric314@gmail.com>
2022-02-27 16:58:03 -0800

    Adjusting formatting, removing console.logs, adding a more time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskContainer = $("#timeBlockContainer");
 
 var defaultTimeSlots = [
+  { time: "8:00", task: "" },
   { time: "9:00", task: "" },
   { time: "10:00", task: "" },
   { time: "11:00", task: "" },
@@ -10,6 +11,8 @@ var defaultTimeSlots = [
   { time: "15:00", task: "" },
   { time: "16:00", task: "" },
   { time: "17:00", task: "" },
+  { time: "18:00", task: "" },
+  { time: "19:00", task: "" },
 ];
 var needDefaultStorage = true;
 var timeTaskStorage = getStorage();
@@ -19,9 +22,9 @@ displayTaskList();
 $(document).ready(function () {
   taskContainer.on("click", "button.saveBtn", function (event) {
     event.preventDefault();
-    var thisSaveEle = $(this).closest('li');
+    var thisSaveEle = $(this).closest("li");
     var time = thisSaveEle.attr("data-time");
-    var task = $(`li[data-time='${time}']`).find('textarea').val()
+    var task = $(`li[data-time='${time}']`).find("textarea").val();
     clickSaveTask(time, task);
   });
 });
@@ -32,48 +35,47 @@ function clickSaveTask(time, task) {
 }
 
 function saveTask(time, task) {
-  console.log("timeTaskStorage: ", timeTaskStorage)
-  if(timeTaskStorage.length === 0) {
-    timeTaskStorage = defaultTimeSlots
+  // console.log("timeTaskStorage: ", timeTaskStorage)
+  if (timeTaskStorage.length === 0) {
+    timeTaskStorage = defaultTimeSlots;
   }
-  for(var i=0; i < timeTaskStorage.length; i++){
-    if(timeTaskStorage[i].time === time){
-      timeTaskStorage[i].task = task
+  for (var i = 0; i < timeTaskStorage.length; i++) {
+    if (timeTaskStorage[i].time === time) {
+      timeTaskStorage[i].task = task;
     }
   }
-  console.log("timeTaskStorage: ", timeTaskStorage)
+  // console.log("timeTaskStorage: ", timeTaskStorage)
 }
 
 function displayTaskList() {
   // var currentStorage = getStorage();
 
-    // Add some date.
-    if(needDefaultStorage === true){
-      for(var i = 0; i < defaultTimeSlots.length; i++){
-        renderRow(defaultTimeSlots[i].time,defaultTimeSlots[i].task)
-      }
-      needDefaultStorage = false
-    } else {
-      for(var i = 0; i < timeTaskStorage.length; i++){
-        renderRow(timeTaskStorage[i].time,timeTaskStorage[i].task)
-      }
+  // Add some date.
+  if (needDefaultStorage === true) {
+    for (var i = 0; i < defaultTimeSlots.length; i++) {
+      renderRow(defaultTimeSlots[i].time, defaultTimeSlots[i].task);
     }
- 
+    needDefaultStorage = false;
+  } else {
+    for (var i = 0; i < timeTaskStorage.length; i++) {
+      renderRow(timeTaskStorage[i].time, timeTaskStorage[i].task);
+    }
+  }
 }
 
-function renderRow(time, task){
-  var timeRowSearch = $(`li[data-time='${time}']`)
-  var timeRowEle = ""
-  var labelEle = ""
-  var textareaEle = ""
-  var buttonEle = ""
+function renderRow(time, task) {
+  var timeRowSearch = $(`li[data-time='${time}']`);
+  var timeRowEle = "";
+  var labelEle = "";
+  var textareaEle = "";
+  var buttonEle = "";
   var rowExists = false;
   var timeArr = time.split(":");
-  var localTime = ""
-  var amPm = ""
-  var timeBgClassStr = getPastPresentFuture(timeArr)
+  var localTime = "";
+  var amPm = "";
+  var timeBgClassStr = getPastPresentFuture(timeArr);
 
-  if(timeArr[0] == 12){
+  if (timeArr[0] == 12) {
     localTime = 12;
     amPm = "PM";
   } else if (timeArr[0] >= 12) {
@@ -83,23 +85,32 @@ function renderRow(time, task){
     localTime = timeArr[0];
     amPm = "AM";
   }
- 
-  
-    // if the row element does not exist, then create new one, else assign
-    // Helps render content once.
-  if(timeRowSearch.length === 0){
+
+  // if the row element does not exist, then create new one, else assign
+  // Helps render content once.
+  if (timeRowSearch.length === 0) {
     rowExists = false;
-    timeRowEle = $(`<li class="row color-bg-secondary form-floating time-block" data-time="${time}">`);
-    labelEle = $(`<label class="col-2 col-lg-1 p-3 hour " >${localTime + ":" + timeArr[1] + " " + amPm}</label>`);
-    textareaEle = $(`<textarea class="col-8 col-lg-10 form-control  ${timeBgClassStr}" placeholder="" >${task}</textarea>`);
-    buttonEle = $('<button class="col-2 col-lg-1 saveBtn"><i class="bi-calendar-plus fa-lg"></i></button>');
+    timeRowEle = $(
+      `<li class="row color-bg-secondary form-floating time-block" data-time="${time}">`
+    );
+    labelEle = $(
+      `<label class="col-2 col-lg-1 p-3 hour " >${
+        localTime + ":" + timeArr[1] + " " + amPm
+      }</label>`
+    );
+    textareaEle = $(
+      `<textarea class="col-8 col-lg-10 form-control  ${timeBgClassStr}" placeholder="" >${task}</textarea>`
+    );
+    buttonEle = $(
+      '<button class="col-2 col-lg-1 saveBtn"><i class="bi-calendar-plus fa-lg"></i></button>'
+    );
   } else {
     rowExists = true;
-    textareaEle = timeRowSearch.find('textarea')
-    textareaEle.val(task)
+    textareaEle = timeRowSearch.find("textarea");
+    textareaEle.val(task);
   }
 
-  if(rowExists === false){
+  if (rowExists === false) {
     labelEle.appendTo(timeRowEle);
     textareaEle.appendTo(timeRowEle);
     buttonEle.appendTo(timeRowEle);
@@ -107,19 +118,22 @@ function renderRow(time, task){
   }
 }
 
-function getPastPresentFuture(timeArr){
-  var pastPresentFuture = ""
+function getPastPresentFuture(timeArr) {
+  var pastPresentFuture = "";
   var current = luxon.DateTime.now().toObject();
-  var rowDateTime = luxon.DateTime.fromObject({hour: timeArr[0], minute: timeArr[1] }).toObject();
+  var rowDateTime = luxon.DateTime.fromObject({
+    hour: timeArr[0],
+    minute: timeArr[1],
+  }).toObject();
 
   // console.log("current: ", current)
   // console.log("rowDateTime: ", rowDateTime)
-  if(current.hour === rowDateTime.hour){
-    pastPresentFuture = "present"
-  } else if (current.hour > rowDateTime.hour){
-    pastPresentFuture = "past"
+  if (current.hour === rowDateTime.hour) {
+    pastPresentFuture = "present";
+  } else if (current.hour > rowDateTime.hour) {
+    pastPresentFuture = "past";
   } else {
-    pastPresentFuture = "future"
+    pastPresentFuture = "future";
   }
 
   return pastPresentFuture;
@@ -130,7 +144,7 @@ function getStorage() {
   if (storageData === null) {
     storageData = [];
   } else {
-    needDefaultStorage = false
+    needDefaultStorage = false;
   }
 
   return storageData;
